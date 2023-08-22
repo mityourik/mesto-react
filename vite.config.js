70f680f8cda6298e2587216172a2e2635a0e467f
Dmitry <mityourik@gmail.com>
2023-08-22 20:55:24 -0300

    fix base in vite.config to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// переменная окружения
-const isGHPagesDeploy = process.env.DEPLOY_GH_PAGES === 'true';
-
 export default defineConfig({
-  base: isGHPagesDeploy ? '/mesto-react/' : '/',
+  base: '/mesto-react/',
   plugins: [react()],
 });
