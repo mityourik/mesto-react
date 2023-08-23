02ab7291659c5a5dfc44137266a4c03fdd7cdedf
Dmitry <mityourik@gmail.com>
2023-08-23 09:58:36 -0300

    fix deploy with base

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/mesto-react/',//если убираю base, то проект не разворачивается на ghPages. не находит входные js и css
   plugins: [react()],
 });
