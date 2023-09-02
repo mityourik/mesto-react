6132ce1a59f4d00ce6c3224e95163c20017e5183
Dmitry <mityourik@gmail.com>
2023-09-02 17:52:40 -0300

    save mesto-react before new proj

--- src/components/ConfirmationPopup.jsx
+++ src/components/ConfirmationPopup.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
 
-function ConfirmationPopup({ isOpen, onClose, onConfirm, isPreloading, textButton }) {
+function ConfirmationPopup({ isOpen, onClose, onConfirm, isPreloading }) {
     
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
